ceefa3c2bfabf021e1fd8ef77217cb904a6d30a4
maniyadg <maniyarasandhanasekaran@gmail.com>
2023-02-22 19:52:52 +0530

    added

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require ("dotenv");
+const db = require('./db/connection')
+const authRoute = require("./routes/auth.js");
+const usersRoute = require("./routes/users.js");
+const hotelsRoute = require("./routes/hotels.js");
+const roomsRoute = require("./routes/rooms.js");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+db()
+
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend.");
+});
